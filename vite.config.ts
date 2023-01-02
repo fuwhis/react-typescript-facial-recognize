a493e5e6d5ac59113c0f9ed8d305e2f6c483dada
fuwhis <fuwhis@brickmate.vn>
2023-01-02 23:47:42 +0700

    update: vite configuration update with resolve alias directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,19 @@ export default defineConfig({
   //   },
   // }
   plugins: [react()],
+  resolve: {
+    alias: {
+      // src: resolve(__dirname, './src'),
+      // styles: resolve(__dirname, './src/styles'),
+      // elements: resolve(__dirname, './src/elements'),
+      // routes: resolve(__dirname, './src/routes'),
+      // layouts: resolve(__dirname, './src/layouts'),
+      // pages: resolve(__dirname, './src/pages'),
+      // atoms: resolve(__dirname, './src/atoms'),
+      // services: resolve(__dirname, './src/services'),
+      // components: resolve(__dirname, './src/components'),
+      // assets: resolve(__dirname, './src/assets'),
+      // utils: resolve(__dirname, './src/utils')
+    }
+  }
 })
