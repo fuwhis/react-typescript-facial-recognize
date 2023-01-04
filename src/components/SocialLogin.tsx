dd37e5ac5fddf8c34bfa18c740aebf7e5c059b72
fuwhis <fuwhis@brickmate.vn>
2023-01-04 14:02:59 +0700

    update code

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import ggIcon from '../assets/icon/google.svg';
 import kakaoIcon from '../assets/icon/kakao.svg'
-const SocialLoginMethods = () => {
 
-    return(
+const SocialLoginMethods = () => {
+    const kakaoLogin = () => {
+        // check Kakao init
+        // if (!Kakao.isInitialized()) {
+        //     Kakao.init(process.env.VITE_PUBLIC_KAKAO_KEY)
+        // }
+    }
+
+    return (
         <div className="w-full flex space-x-4">
             <button className="flex justify-center border-green-500 bg-transparent h-12">
-                <img alt="" src={ggIcon} width="27px" height="28px"/>
+                <img alt="" src={ggIcon} width="27px" height="28px" />
                 &nbsp; Google
             </button>
 
-            <button className="flex justify-center border-green-500 bg-transparent h-12">
-                <img alt="" src={kakaoIcon} width="27px" height="27px"/>
+            <button className="flex justify-center border-green-500 bg-transparent h-12" onClick={kakaoLogin}>
+                <img alt="" src={kakaoIcon} width="27px" height="27px" />
                 &nbsp; Kakao
             </button>
-
         </div>
     )
 }
