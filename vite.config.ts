720abacd026038d95f5ea1891f77fb75237da3d8
fuwhis <fuwhis@brickmate.vn>
2023-01-06 15:42:40 +0700

    refactor project's configuration

--- vite.config.ts
+++ vite.config.ts
@@ -16,17 +16,17 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // src: resolve(__dirname, './src'),
-      // styles: resolve(__dirname, './src/styles'),
-      // elements: resolve(__dirname, './src/elements'),
-      // routes: resolve(__dirname, './src/routes'),
-      // layouts: resolve(__dirname, './src/layouts'),
-      // pages: resolve(__dirname, './src/pages'),
-      // atoms: resolve(__dirname, './src/atoms'),
-      // services: resolve(__dirname, './src/services'),
-      // components: resolve(__dirname, './src/components'),
-      // assets: resolve(__dirname, './src/assets'),
-      // utils: resolve(__dirname, './src/utils')
+      src: resolve(__dirname, './src'),
+      styles: resolve(__dirname, './src/styles'),
+      elements: resolve(__dirname, './src/elements'),
+      routes: resolve(__dirname, './src/routes'),
+      layouts: resolve(__dirname, './src/layouts'),
+      pages: resolve(__dirname, './src/pages'),
+      atoms: resolve(__dirname, './src/atoms'),
+      services: resolve(__dirname, './src/services'),
+      components: resolve(__dirname, './src/components'),
+      assets: resolve(__dirname, './src/assets'),
+      utils: resolve(__dirname, './src/utils')
     }
   }
 })
