931e75718f52474db585d7c128d96f0663245678
fuwhis <fuwhis@brickmate.vn>
2023-01-06 15:44:45 +0700

    refactor module routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
-import '../src/assets/css/global.scss'
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { RecoilRoot } from 'recoil'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
+import '../src/assets/scss/global.scss'
+import App from './App'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-  {/* <BrowserRouter> */}
+  // <React.StrictMode>
+  <BrowserRouter>
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       </QueryClientProvider>
     </RecoilRoot>
-  {/* </BrowserRouter> */}
-  </React.StrictMode>
+  </BrowserRouter>
+  // </React.StrictMode>
 )
