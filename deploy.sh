5664a5b689f7236111f117fd303d7783ca14c3ff
fuwhis <fuwhis@brickmate.vn>
2023-01-09 01:21:53 +0700

    update: modify curl of my repo on pushing action

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:fuwhis/fuwhis.github.io.git master
+# git push -f git@github.com:fuwhis/react-typescript-facial-recognize.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
