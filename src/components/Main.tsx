d3239f64ba1b043aebb3fcfb8166418a1f879c19
fuwhis <fuwhis@brickmate.vn>
2023-01-08 23:29:33 +0700

    refactor style on splitted modules

---
+++ src/components/Main.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "../pages/Home";
+import SignUp from "../pages/SignUp";
+
+const ContextApp = () => {
+    return (
+        <div className='min-h-min flex justify-center items-center lg:py-12 sm:py-6 px-2 sm:px-6 lg:px-8'>
+            <div className="max-w-md w-full space-y-8">
+                {/* <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='sign-up' element={<SignUp />} />
+                    </Routes>
+                </BrowserRouter> */}
+                <Home/>
+            </div>
+        </div>
+    );
+}
+
+export default ContextApp;
