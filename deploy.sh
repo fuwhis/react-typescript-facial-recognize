92b11c2f741c3f1f247f904e76c5676963a3a308
fuwhis <fuwhis@brickmate.vn>
2023-01-09 01:20:19 +0700

    update: addition a static deploy to github

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output
+cd dist
+
+# place .nojekyll to bypass Jekyll processing 
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B master
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:fuwhis/fuwhis.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
